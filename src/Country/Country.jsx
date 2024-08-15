27e7f7e81dba97f5c3b0e1042c3bdbaa763acb07
Poojitha28 <poojithavudhayagiri28@gmail.com>
2024-08-15 22:26:37 +0530

    added search functionality

--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -4,20 +4,35 @@ import axios from "axios";
 function Country()
 {
     const [data,setData] = new useState([]);
+    const [filterData,setFileterData] = new useState([]);
+    const debounceSearch=(value)=>{
+        let timerId;
+        if (timerId) {
+            clearTimeout(timerId);
+        }
+
+        timerId = setTimeout(() => {
+            const filteredData = data.filter(x => x.name.common.toLowerCase().includes(value.toLowerCase()));
+        
+            setFileterData(filteredData);
+            }, 1000);
+
+    }
     useEffect( ()=>{
         try{
         const fetchData= async () => {
             try
             {
-            const data= await axios.get('https://xcountries-backend.azurewebsites.net/all');
+            const data= await axios.get('https://restcountries.com/v3.1/all');
             setData(data.data);
+            setFileterData(data.data);
             }catch(err)
             {
                 console.error("Error fetching data:", err);
             }
         }
         fetchData();
-        }
+        }       
         catch(err)
         {
             console.error(err);
@@ -25,11 +40,13 @@ function Country()
 
     },[]);
     return(
+        <div>
+            <input type="text" style={{position:"relative",left:"450px",marginBottom:"10px",width:"500px"}} onChange={(e)=>{debounceSearch(e.target.value)}} />
         <div style={{display:"flex",
             flexFlow:"wrap",
             gap:"30px",
         }}>
-          {data.map((ele,idx) => ( 
+          {filterData.map((ele,idx) => ( 
             <div key={idx} style={
                 {
                   display:"flex",
@@ -44,11 +61,12 @@ function Country()
                   padding:"5px"
                 }
             }>   <div style={{width:"100px",height:"100px"}}> 
-                <img src={ele.flag} alt="Text" style={{maxWidth:"100%",maxHeight:"100%",objectFit:"fill"}} />
+                <img src={ele.flags.svg} alt="Text" style={{maxWidth:"100%",maxHeight:"100%",objectFit:"fill"}} />
                 </div>
-                <h6 style={{padding:"3px"}}>{ele.name}</h6>
+                <h6 style={{padding:"3px"}}>{ele.name.common}</h6>
             </div>
           ))}
+        </div>
         </div>
     )
  
